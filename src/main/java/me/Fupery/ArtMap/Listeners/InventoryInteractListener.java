8c916881b29c382a016d90ef93aef297a9797d1b
Fupery <Fupery1@gmail.com>
2015-11-19 10:50:01 +1100

    Prevent recipe menu item dupe

--- src/main/java/me/Fupery/ArtMap/Listeners/InventoryInteractListener.java
+++ src/main/java/me/Fupery/ArtMap/Listeners/InventoryInteractListener.java
@@ -2,6 +2,7 @@ package me.Fupery.ArtMap.Listeners;
 
 import me.Fupery.ArtMap.ArtMap;
 import me.Fupery.ArtMap.Utils.Preview;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
@@ -9,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryInteractListener implements Listener {
 
@@ -23,6 +25,7 @@ public class InventoryInteractListener implements Listener {
     public void onPlayerItemHeld(PlayerItemHeldEvent event) {
         checkPreviewing(event.getPlayer(), event);
     }
+
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
@@ -41,8 +44,8 @@ public class InventoryInteractListener implements Listener {
     private void checkPreviewing(Player player, Cancellable event) {
 
         if (plugin.isPreviewing(player)) {
-
             event.setCancelled(true);
+            player.setItemOnCursor(new ItemStack(Material.AIR));
             Preview.stop(plugin, player);
         }
     }
