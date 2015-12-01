dd1ad4a02e619d1e6e65d395977c5fadf2b1f88e
Fupery <Fupery1@gmail.com>
2015-12-01 21:01:50 +1100

    Pre-Generate help text

--- src/main/java/me/Fupery/ArtMap/Command/CommandHelp.java
+++ src/main/java/me/Fupery/ArtMap/Command/CommandHelp.java
@@ -15,6 +15,10 @@ import static me.Fupery.ArtMap.Utils.Formatting.helpLine;
 
 public class CommandHelp extends ArtMapCommand {
 
+    private final String[] text = getLines();
+    private final TextComponent adminFooter = getFooter(true);
+    private final TextComponent playerFooter = getFooter(false);
+
     CommandHelp(ArtMap plugin) {
         super(null, "/artmap help", true);
         this.plugin = plugin;
@@ -22,37 +26,47 @@ public class CommandHelp extends ArtMapCommand {
 
     @Override
     public boolean runCommand(CommandSender sender, String[] args, ReturnMessage msg) {
+
         MultiLineReturnMessage multiMsg =
                 new MultiLineReturnMessage(sender, ArtMap.Lang.HELP_HEADER.message());
-        multiMsg.setLines(new String[]{
+
+        multiMsg.setLines(text);
+
+        if (sender instanceof Player) {
+            multiMsg.setFooter(getFooter(sender.hasPermission("artmap.admin")));
+        }
+        Bukkit.getScheduler().runTask(plugin, multiMsg);
+        return true;
+    }
+
+    private String[] getLines() {
+        return new String[]{
                 helpLine("/artmap save <title>", "save your artwork"),
                 helpLine("/artmap delete <title>", "delete your artwork"),
                 helpLine("/artmap preview <title>", "preview an artwork"),
                 helpLine("/artmap list [player|all] [pg]", "list artworks"),
-                ArtMap.Lang.HELP_MESSAGE.rawMessage()
-        });
-        if (sender instanceof Player) {
-            String hoverMessage = sender.hasPermission("artmap.admin") ?
-                    ArtMap.Lang.GET_ITEM.rawMessage() : ArtMap.Lang.RECIPE_HOVER.rawMessage();
+                ArtMap.Lang.HELP_MESSAGE.rawMessage()};
+    }
 
-            ArtMaterial[] recipes = ArtMaterial.values(true);
+    private TextComponent getFooter(boolean admin) {
+        String hoverMessage = admin ?
+                ArtMap.Lang.GET_ITEM.rawMessage() : ArtMap.Lang.RECIPE_HOVER.rawMessage();
 
-            TextComponent footer = new TextComponent(ChatColor.AQUA + "Recipes: ");
-            TextComponent[] items = new TextComponent[recipes.length];
+        ArtMaterial[] recipes = ArtMaterial.values(true);
 
-            for (int i = 0; i < items.length; i++) {
-                String title = recipes[i].name().toLowerCase();
-                items[i] = new TextComponent(String.format(ArtMap.Lang.RECIPE_BUTTON.rawMessage(),
-                        title) + ChatColor.GOLD + " | ");
-                items[i].setClickEvent(
-                        new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/artmap get " + title));
-                items[i].setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder(String.format(hoverMessage, title)).create()));
-                footer.addExtra(items[i]);
-            }
-            multiMsg.setFooter(footer);
+        TextComponent footer = new TextComponent(ChatColor.AQUA + "Recipes: ");
+        TextComponent[] items = new TextComponent[recipes.length];
+
+        for (int i = 0; i < items.length; i++) {
+            String title = recipes[i].name().toLowerCase();
+            items[i] = new TextComponent(String.format(ArtMap.Lang.RECIPE_BUTTON.rawMessage(),
+                    title) + ChatColor.GOLD + " | ");
+            items[i].setClickEvent(
+                    new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/artmap get " + title));
+            items[i].setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                    new ComponentBuilder(String.format(hoverMessage, title)).create()));
+            footer.addExtra(items[i]);
         }
-        Bukkit.getScheduler().runTask(plugin, multiMsg);
-        return true;
+        return footer;
     }
 }
