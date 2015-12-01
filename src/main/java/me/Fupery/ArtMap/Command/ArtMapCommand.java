c8a2bda0b6b29af24891646f51645ac4222b0c5a
Fupery <Fupery1@gmail.com>
2015-12-01 20:25:17 +1100

    Infer min and max args from usage

---
+++ src/main/java/me/Fupery/ArtMap/Command/ArtMapCommand.java
@@ -0,0 +1,68 @@
+package me.Fupery.ArtMap.Command;
+
+import me.Fupery.ArtMap.ArtMap;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
+
+import static me.Fupery.ArtMap.Utils.Formatting.playerError;
+
+interface AbstractCommand {
+    boolean runCommand(CommandSender sender, String[] args, ReturnMessage msg);
+}
+
+public abstract class ArtMapCommand implements AbstractCommand {
+
+    String usage;
+    private AbstractCommand ArtMapCommand = this;
+    private String permission;
+    private int minArgs;
+    private int maxArgs;
+    private boolean consoleAllowed;
+    ArtMap plugin;
+
+    ArtMapCommand(String permission, String usage, boolean consoleAllowed) {
+        this.permission = permission;
+        this.consoleAllowed = consoleAllowed;
+
+        if (usage == null) {
+            throw new IllegalArgumentException("Usage must not be null");
+        }
+        String[] args = usage.replace("/artmap ", "").split("\\s+");
+        maxArgs = args.length;
+        minArgs = maxArgs - StringUtils.countMatches(usage, "[");
+        this.usage = usage;
+    }
+
+    void runPlayerCommand(final CommandSender sender, final String args[]) {
+
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+
+            @Override
+            public void run() {
+
+                ReturnMessage returnMsg = new ReturnMessage(sender, null);
+
+                if (permission != null && !sender.hasPermission(permission)) {
+                    returnMsg.message = ArtMap.Lang.NO_PERM.message();
+
+                } else if (!consoleAllowed && !(sender instanceof Player)) {
+                    returnMsg.message = ArtMap.Lang.NO_CONSOLE.message();
+
+                } else if (args.length < minArgs || args.length > maxArgs) {
+                    returnMsg.message = playerError(usage);
+
+                } else if (ArtMapCommand.runCommand(sender, args, returnMsg)) {
+                    return;
+                }
+
+                if (returnMsg.message != null) {
+                    Bukkit.getScheduler().runTask(plugin, returnMsg);
+                }
+            }
+        });
+    }
+}
+
