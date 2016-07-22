d9cb1f6264732896a055fe428b420d765512f95f
Fupery <Fupery1@gmail.com>
2016-07-22 20:34:52 +1000

    Add system to cache player channel for outgoing packets

---
+++ src/main/java/me/Fupery/ArtMap/Protocol/Channel/CacheablePlayerChannel.java
@@ -0,0 +1,36 @@
+package me.Fupery.ArtMap.Protocol.Channel;
+
+import io.netty.channel.Channel;
+import me.Fupery.ArtMap.Utils.Reflection;
+import org.bukkit.Bukkit;
+
+import java.util.UUID;
+
+class CacheablePlayerChannel {
+
+    private final UUID player;
+    private final long expiryTime;
+    private final Channel channel;
+
+    CacheablePlayerChannel(UUID player, long timeToLive) {
+        this.player = player;
+        expiryTime = (timeToLive == -1) ? -1 : System.currentTimeMillis() + timeToLive;
+        channel = Reflection.getPlayerChannel(Bukkit.getPlayer(player));
+    }
+
+    public void sendPacket(Object packet) {
+        channel.pipeline().writeAndFlush(packet);
+    }
+
+    boolean isExpired() {
+        return expiryTime != -1 && expiryTime >= System.currentTimeMillis();
+    }
+
+    Channel getChannel() {
+        return channel;
+    }
+
+    public UUID getPlayer() {
+        return player;
+    }
+}
