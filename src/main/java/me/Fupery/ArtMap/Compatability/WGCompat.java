7ff2a781834048a7056cfddd8f92aa3005cb62d6
Fupery <Fupery1@gmail.com>
2016-08-09 13:26:48 +1000

    World guard handling change - Allow use of all actions if player has build perms

--- src/main/java/me/Fupery/ArtMap/Compatability/WGCompat.java
+++ src/main/java/me/Fupery/ArtMap/Compatability/WGCompat.java
@@ -1,10 +1,12 @@
 package me.Fupery.ArtMap.Compatability;
 
 import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.blacklist.action.Action;
 import com.sk89q.worldguard.bukkit.WGBukkit;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -19,9 +21,8 @@ public class WGCompat implements InteractPermissionHandler {
     public boolean checkActionAllowed(Player player, Location location, InteractAction action) {
         if (!isLoaded()) return true;
         WorldGuardPlugin wg = ((WorldGuardPlugin) worldGuardPlugin);
-        if (action == InteractAction.BUILD) {
-            return wg.canBuild(player, location);
-        }
+        if (wg.canBuild(player, location)) return true;
+        else if (action == InteractAction.BUILD) return false;
         ApplicableRegionSet set = wg.getRegionContainer().createQuery().getApplicableRegions(location);
         LocalPlayer localPlayer = wg.wrapPlayer(player);
         if (action == InteractAction.INTERACT) {
