f1b0689d35b5ec395e466ab076b084009345cbe8
Fupery <Fupery1@gmail.com>
2017-04-22 00:58:30 +1000

    Add checks for valid config values

--- src/main/java/me/Fupery/ArtMap/Config/Configuration.java
+++ src/main/java/me/Fupery/ArtMap/Config/Configuration.java
@@ -17,8 +17,10 @@ public class Configuration {
     public Configuration(ArtMap plugin, CompatibilityManager manager) {
         FileConfiguration configuration = plugin.getConfig();
         this.LANGUAGE = configuration.getString("language");
-        this.WORLD = configuration.getString("world");
-        this.ARTWORK_AUTO_SAVE = configuration.getInt("artworkAutoSave");
+        String world = configuration.getString("world");
+        this.WORLD = world != null ? world : "world";
+        int autoSave = configuration.getInt("artworkAutoSave");
+        this.ARTWORK_AUTO_SAVE = autoSave >= 30 ? autoSave : 300;
         this.SWEAR_FILTER = configuration.getBoolean("swearFilter");
         this.DISABLE_ACTION_BAR = configuration.getBoolean("disableActionBar");
         this.CUSTOM_RECIPES = configuration.getBoolean("customRecipes");
