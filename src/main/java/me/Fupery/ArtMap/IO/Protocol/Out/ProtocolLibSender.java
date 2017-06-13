70d4cb122596a865eff314638fbf86f02dad6986
Fupery <Fupery1@gmail.com>
2017-06-13 19:30:25 +1000

    Add compatibility for ProtocolLib 4.3.0

--- src/main/java/me/Fupery/ArtMap/IO/Protocol/Out/ProtocolLibSender.java
+++ src/main/java/me/Fupery/ArtMap/IO/Protocol/Out/ProtocolLibSender.java
@@ -3,20 +3,48 @@ package me.Fupery.ArtMap.IO.Protocol.Out;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import me.Fupery.ArtMap.ArtMap;
 import me.Fupery.ArtMap.IO.ErrorLogger;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
 
+import static me.Fupery.ArtMap.Utils.VersionHandler.BukkitVersion.v1_12;
+
 public class ProtocolLibSender implements PacketSender {
+
+    private PacketBuilder builder = ArtMap.getBukkitVersion().getVersion().isGreaterOrEqualTo(v1_12)
+            ? new ChatPacketBuilder() : new ChatPacketBuilderLegacy();
+
+    interface PacketBuilder {
+        PacketContainer buildChatPacket(String message);
+    }
+
+    private class ChatPacketBuilderLegacy implements PacketBuilder {
+        @Override
+        public PacketContainer buildChatPacket(String message) {
+            PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.CHAT);
+            packet.getChatComponents().write(0, WrappedChatComponent.fromText(message));
+            packet.getBytes().write(0, (byte) 2);
+            return packet;
+        }
+    }
+
+    private class ChatPacketBuilder implements PacketBuilder {
+        @Override
+        public PacketContainer buildChatPacket(String message) {
+            PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.CHAT);
+            packet.getChatComponents().write(0, WrappedChatComponent.fromText(message));
+            packet.getChatTypes().write(0, EnumWrappers.ChatType.GAME_INFO);
+            return packet;
+        }
+    }
+
     @Override
     public WrappedPacket buildChatPacket(String message) {
-        PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.CHAT);
-        packet.getChatComponents().write(0, WrappedChatComponent.fromText(message));
-        packet.getBytes().write(0, (byte) 2);
-
-        return new WrappedPacket<PacketContainer>(packet) {
+        return new WrappedPacket<PacketContainer>(builder.buildChatPacket(message)) {
             @Override
             public void send(Player player) {
                 try {
